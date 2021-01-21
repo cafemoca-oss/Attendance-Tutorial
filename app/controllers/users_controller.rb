90eb0606b337830756d106d0236205c0d14b7af6
cafemoca-oss <yu.purumeria02@gmail.com>
2021-01-21 10:07:22 +0000

    Finish signup-user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,7 +11,8 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      
+      flash[:success] = '新規作成に施工しました。'
+      redirect_to @user
     else
       render :new
     end  
